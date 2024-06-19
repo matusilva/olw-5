7a1ba33dbc508ce0f239eff3ab8637fb599531a2
matusilva <viniciusmatheus.dev@gmail.com>
2024-06-18 21:59:58 -0300

    feat: criar models, migrations e relacionamentos das tabelas e popular banco de dados com seeders e dados fakes

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'slug',
+        'brand_id',
+        'category_id',
+        'is_featured'
+    ];
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function skus(): HasMany
+    {
+        return $this->hasMany(Sku::class);
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'role_id' => fake()->numberBetween(1, 2),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
