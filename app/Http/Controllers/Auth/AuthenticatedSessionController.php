470567ff2a39628549e6779406874c0b09039535
matusilva <viniciusmatheus.dev@gmail.com>
2024-06-18 22:32:21 -0300

    feat: instalar Breeze e criar api e autenticação com Sanctum para API Only

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $token = auth()->user()->createToken('access_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['mesage' => 'Token revoked']);
+    }
+}
