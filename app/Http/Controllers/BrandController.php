4ae734e652b34dda83f88994597bd170f8ea0d25
matusilva <viniciusmatheus.dev@gmail.com>
2024-06-18 23:24:26 -0300

    feat: criar crud de brands/marcas via api

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandStoreRequest;
+use App\Http\Requests\BrandUpdateRequest;
 use App\Models\Brand;
+use App\Services\BrandServices;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+
+    public function __construct(protected BrandServices $brandServices) {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $brands = $this->brandServices->list();
+
+        return response()->json($brands);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BrandStoreRequest $request)
     {
-        //
+        $brand = $this->brandServices->store($request);
+
+        return response()->json($brand);
     }
 
     /**
@@ -28,15 +38,17 @@ class BrandController extends Controller
      */
     public function show(Brand $brand)
     {
-        //
+        return response()->json($brand);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Brand $brand)
+    public function update(BrandUpdateRequest $request, Brand $brand)
     {
-        //
+        $brand = $this->brandServices->update($request, $brand);
+        
+        return response()->json($brand);
     }
 
     /**
@@ -44,6 +56,8 @@ class BrandController extends Controller
      */
     public function destroy(Brand $brand)
     {
-        //
+        $this->brandServices->destroy($brand);
+
+        return response()->json(["message" => "Successfully deleted"]);
     }
 }
